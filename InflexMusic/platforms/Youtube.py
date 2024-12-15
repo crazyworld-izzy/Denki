7611ad43b6e43573319901b07ed3650e25a551cf
THE ARCHITECT <markusmarwin00@gmail.com>
2024-12-15 18:12:48 +0530

    Update Youtube.py

--- InflexMusic/platforms/Youtube.py
+++ InflexMusic/platforms/Youtube.py
@@ -9,8 +9,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from AmritaXMusic.utils.database import is_on_off
-from AmritaXMusic.utils.formatters import time_to_seconds
+from InflexMusic.utils.database import is_on_off
+from InflexMusic.utils.formatters import time_to_seconds
 
 async def shell_cmd(cmd):
     proc = await asyncio.create_subprocess_shell(
